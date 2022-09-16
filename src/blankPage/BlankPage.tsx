c02ab1fc334876cdb9fc67586488220c3db65d79
tomscambler <tomscambler@gmail.com>
2022-09-16 16:54:10 +0100

    TS-00: Added paper background and button

---
+++ src/blankPage/BlankPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./BlankPage.css";
+// import { Link } from "react-router-dom";
+
+
+export const BlankPage: React.FunctionComponent = () => {
+    return(
+        <></>
+    )
+}
